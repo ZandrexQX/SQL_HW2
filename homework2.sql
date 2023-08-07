59195b8c852ab5d9aeefe67e884d1aeb38615ba8
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-08-07 22:56:40 +0300

    Add files via upload

--- homework2.sql
+++ homework2.sql
@@ -1,8 +1,6 @@
 select * from sales;
-
 alter table homework2.sales
 add column class varchar(100);
-
 update homework2.sales 
 set class = case when order < 100 then '0 to 100'
 				 when order between 100 and 300 then '100 to 300'
@@ -17,3 +15,5 @@ select id,
 			 when order > 300 then 'more 300'
 			 else 'FAIL' end as class
 from sales;
+
+select * from orders;
